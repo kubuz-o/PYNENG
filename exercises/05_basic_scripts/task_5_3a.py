871f5654f47ee0d285365e6c010b5fda752e2fd6
kubuz-o <kubuz.oleg@gmail.com>
2020-10-10 18:36:30 +0300

    Task 5.3a done

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -24,3 +24,20 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+data_mode = input("Введите режим работы интерфейса (access/trunk): ")
+data_interface = input("Введите тип и номер интерфейса: ")
+
+port_variable = {'access': 'Введите номер VLAN: ',         # создаем дополнительный словарь
+                 'trunk': 'Введите разрешенные VLANы: '}
+data_vlan = input(port_variable[data_mode])
+
+d_keys = ['access', 'trunk']  # ключи основного словаря - режим работы интерфейса
+template = dict.fromkeys(d_keys)  # создаем основной словарь
+# заполняем основной словарь шаблонами и значениями из дополнительного словаря
+template['access'] = [port_variable['access'], access_template]
+template['trunk'] = [port_variable['trunk'], trunk_template]
+
+template_chosen = template[data_mode]  # выбранный шаблон по введенному режиму работы интерфейса
+config = '\n'.join(template_chosen[1])  # шаблон на вывод
+
+print('interface', data_interface, '\n', config.format(data_vlan))  # вывод конфига
